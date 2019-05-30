5dd90d84fa4d5bd2e828b7c32a79c67e26658006
Johann Schmitz <johann@j-schmitz.net>
2019-05-30 08:15:16 +0200

    Properly add/remove entries to hosts file

--- docker_events.py
+++ docker_events.py
@@ -1,77 +1,41 @@
 # -*- coding: utf-8 -*-
 import json
+from argparse import ArgumentParser
 
 import daiquiri
 import docker
 import logging
 
-daiquiri.setup(level=logging.DEBUG)
+from python_hosts import Hosts, HostsEntry
+
+daiquiri.setup(level=logging.WARNING)
 logging.getLogger("docker").setLevel(logging.INFO)
 logging.getLogger("urllib3").setLevel(logging.INFO)
 
 logger = logging.getLogger(__name__)
-    #
-    #
-    # status = data.get('status', '')
-    #
-    # if status == "start":
-    #     container_id = data.get('id', '')
-    #     if not container_id:
-    #         continue
-    #
-    #     cfg = client.containers.get(container_id)
-    #     #cfg = client.configs.get(container_id)
-    #     print("---")
-    #     print(cfg)
-    #     print("---")
-
-# https://github.com/jonhadfield/python-hosts
-# from python_hosts.hosts import Hosts, HostsEntry
-# hosts = Hosts(path='hosts_test')
-# new_entry = HostsEntry(entry_type='ipv4', address='1.2.3.4', names=['www.example.com', 'example'])
-# hosts.add([new_entry])
-# hosts.write()
 
 
 class DockerHosts(object):
 
-    def __init__(self, container_filter=None, network_filter=None):
-        self.container_filter = [lambda name: name == x for x in container_filter] if container_filter else [lambda name: True]
-        self.network_filter = [lambda name: name == x for x in network_filter] if network_filter else [lambda name: True]
+    def __init__(self, container_filter=None, network_filter=None, hosts_file=None, pattern=None):
+        self.pattern = pattern
+        self.hosts_file = hosts_file
+        self.container_filter = lambda name: name in container_filter if container_filter else lambda name: True
+        self.network_filter = lambda name: name in network_filter if network_filter else lambda name: True
         
         self.client = docker.from_env()
         self.containers = {}
-        self._update_container_info()
+        self.update_container_info()
 
-    def _update_container_info(self):# todo: remove me
-        containers = self.client.containers.list()
-        
-        def _extract_data():
-            for c in containers:
-                
-                if not self.is_watched_container(c.name):
-                    logger.debug("Ignoring: %s", c.name)
-                    continue
-                
-                logger.debug("Watched container: %s (%s)", c.id, c.name)
-                yield c.id, {
-                    'name': c.name,
-                    'hostname': containers[0].attrs.get('Config', {}).get('Hostname', ''),
-                    'networks': dict([
-                        (network_name, network_config.get('IPAddress', None)) for network_name, network_config in c.attrs.get('NetworkSettings', {}).get('Networks', {}).items()
-                    ])
-                }
-        self.containers = dict(_extract_data())
-        logger.debug("Containers: %s", self.containers)
-    
     def update_container_info(self, container_id=None):
         
         query_filters = {'id': container_id} if container_id else None
-        
+
+        previous_config = self.containers if container_id is None else self.containers.get(container_id)
+
         for c in self.client.containers.list(query_filters):
         
             if not self.is_watched_container(c.name):
-                logger.debug("Ignoring: %s", c.name)
                 continue
         
             logger.debug("Watched container: %s (%s)", c.id, c.name)
@@ -87,16 +51,60 @@ class DockerHosts(object):
         logger.info("Containers:")
         for cid, vars in self.containers.items():
             logger.info("%s: %s", cid, vars)
-    
+        
+        self.write_hosts_entries(container_id, previous_config)
+
+    def write_hosts_entries(self, container_id, previous_config):
+        """
+        :param container_id: May be None (= all containers)
+        :param previous_config: Either None (no information available), a dict from self.containers or the value of self.containers. If
+        it is a dict of a single container, container_id must be set. If it is the previous value of self.containers, container_id MUST NOT be set
+        """
+        if container_id:
+            self.write_file_entry(self.containers.get(container_id, None), previous_config)
+        else:
+            for cid in set(list(self.containers.keys()) + list(previous_config.keys())):
+                self.write_file_entry(self.containers.get(cid, None), previous_config.get(container_id, None))
+
+    def write_file_entry(self, current_config=None, previous_config=None):
+        f = Hosts(self.hosts_file)
+
+        def _fmt(**kwargs):
+            return self.pattern.format(**kwargs)
+
+        def _gen_entries():
+            for network_name, network_address in current_config.get('networks', {}).items():
+                name = _fmt(name=current_config['name'], hostname=current_config['hostname'], network=network_name)
+                logger.debug("Adding host entry %s <> %s", network_address, name)
+                yield HostsEntry(entry_type='ipv4', address=network_address, names=[name])
+
+        for cfg in current_config, previous_config:
+            if cfg is None:
+                continue
+                
+            for _, addr in cfg.get('networks', {}).items():
+                logger.debug("Removing entries matching address: %s", addr)
+                f.remove_all_matching(address=addr)
+
+            for network, _ in cfg.get('networks', {}).items():
+                name = _fmt(name=cfg['name'], hostname=cfg['hostname'], network=network)
+                logger.debug("Removing entries matching name: %s", name)
+                f.remove_all_matching(name=name)
+
+        if current_config:
+            f.add(list(_gen_entries()))
+
+        f.write()
+
     def is_watched_container(self, container_name):
-        return any([f(container_name) for f in self.container_filter])
+        return self.container_filter(container_name)
     
     def is_watched_network(self, network_name):
-        return any([f(network_name) for f in self.network_filter])
+        return self.network_filter(network_name)
     
     def register_container(self, container_id, container_name):
         if container_id not in self.containers:
-            logger.info("Registring new container: %s (%s)", container_name, container_id)
+            logger.debug("Registring new container: %s (%s)", container_name, container_id)
             self.containers[container_id] = {
                 'name': container_name
             }
@@ -104,24 +112,13 @@ class DockerHosts(object):
     def deregister_container(self, container_id):
         if container_id in self.containers:
             logger.info("De-registring container: %s", container_id)
+            previous_config = self.containers.get(container_id, None)
             del self.containers[container_id]
-    
-    def dump_container_infos(self, log_mark):
-        logger.warning("------ (%s)", log_mark)
-        for c in self.client.containers.list():
-            logger.warning("%s -> %s", c.id, {
-                'name': c.name,
-                'hostname': c.attrs.get('Config', {}).get('Hostname', ''),
-                'networks': dict([
-                    (network_name, network_config.get('IPAddress', None)) for network_name, network_config in
-                    c.attrs.get('NetworkSettings', {}).get('Networks', {}).items()
-                ])
-            })
-    
+            self.write_hosts_entries(container_id, previous_config)
+       
     def run(self):
         for e in self.client.events():
             data = json.loads(e)
-            logger.debug("Event: %s", data)
 
             status = data.get('status', '')
             cid = data.get('id', '')
@@ -132,50 +129,28 @@ class DockerHosts(object):
             actor_attributes = actor.get('Attributes', {})
             actor_container_name = actor_attributes.get('name', '')
 
-            if not cid or not self.is_watched_container(actor_container_name):
-                continue
-            
-            if status == "create":
+            if status == "create" and self.is_watched_container(actor_container_name):
                 self.register_container(cid, actor_container_name)
+                continue
+
             if status == "die":
                 self.deregister_container(cid)
+                continue
+
             if status == "start" and cid in self.containers:
                 self.update_container_info(cid)
+                continue
             
-            # fixme: don't rely on self.client.containers.list()!!
-
             container_id = actor_attributes.get('container', '')
             network_name = actor_attributes.get('name', '')
             if type == "network" and not self.is_watched_network(network_name):
-                logger.warning("Ignoring (unwatched network): %s", data)
+                logger.warning("Ignoring (unwatched network '%s'): %s", network_name, data)
                 continue
             
             if action == 'connect' or action == 'disconnect':
-                #logger.info("EVENT: Container %s on network %s: %s", container_id, network_name, action)
-                
                 if container_id in self.containers:
-                    logger.warning("Network event on WATCHED CONTAINER: %s (%s): %s", container_id, action, self.containers.get(container_id))
-                    self.dump_container_infos("network connect/disconnect")
-                    
-                
-                
-            #     # self._update_container_info()
-            #
-            #     attributes = data.get('Actor', {}).get('Attributes', {})
-            #     container_id = attributes.get('container', '')
-            #     network_name = attributes.get('name', '')
-            #
-            #     logger.info("EVENT: Container %s on network %s: %s", container_id, network_name, action)
-            #
-            #     if container_id not in self.containers:
-            #         logger.info("Ignoring network event for container: %s", container_id)
-            #         continue
-            #
-            #     if action == 'connect':
-            #         self.container_connected(container_id, network_name)
-            #     else:
-            #         self.container_disconnected(container_id, network_name)
-    
+                    self.update_container_info(container_id=container_id)
+
     def container_connected(self, container_id, network_name):
         logger.info("Container %s connected to network %s", container_id, network_name)
 
@@ -191,4 +166,21 @@ class DockerHosts(object):
         if network_name in self.containers[container_id]['networks']:
             del self.containers[container_id]['networks'][network_name]
 
-DockerHosts(container_filter=['dummy', 'dummy2'], network_filter=['bridge', 'test']).run()
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('-f', '--hosts-file', default="/etc/hosts", help="the hosts-style file to write to (default: %(default)s)")
+    parser.add_argument('-p', '--pattern', default="{hostname}.{network}.local", help="the host entry pattern (default: %(default)s)")
+    parser.add_argument('--container-filter', type=str, nargs='*')
+    parser.add_argument('--network-filter', type=str, nargs='*')
+    parser.add_argument('-v', '--verbose', action='count', help="Increase verbosity (default: %(default)s)", default=3)
+
+    args = parser.parse_args()
+
+    daiquiri.setup(level=40 - ((int(args.verbose) - 1) * 10))
+
+    c = DockerHosts(container_filter=args.container_filter,
+                network_filter=args.network_filter,
+                hosts_file=args.hosts_file,
+                pattern=args.pattern)
+    c.run()
